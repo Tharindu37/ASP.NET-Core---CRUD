0d2f0da5e5530690a78d59cff3a98752ef34a877
Tharindu37 <tharindut520@gmail.com>
2024-12-26 03:07:51 +0530

    update model

--- crud.Models/User.cs
+++ crud.Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,16 @@ namespace crud.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
         public string Name { get; set; }
+        [MaxLength(10)]
+        public string AddressNo { get; set; }
+        [MaxLength(250)]
+        public string Street { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string City { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
